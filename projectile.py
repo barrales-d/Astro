8c5477a37738c4848055337313627e09de90b78b
barrales-d <dbarrales06@gmail.com>
2023-09-29 15:36:55 -0700

    new projectile Manager system

--- projectile.py
+++ projectile.py
@@ -2,6 +2,9 @@ import pygame
 import math
 from settings import *
 from colors import *
+from src.animator import *
+
+from random import randint, choice
 
 PROJECTILE_DESPAWN_TIMER = 10
 class ProjectileKind():
@@ -59,3 +62,33 @@ class Projectile(pygame.sprite.Sprite):
             self.rect.y = self.unrotated_rect.y
 
         self.destroy()
+
+class ProjectileManager:
+    def __init__(self):
+        self.animtor = Animator('./graphics/asteroid_spritesheet_outline.png', 32, 1, 3, (0, 0))
+
+        self.animtor.add('asteroid1', 1, 1, 1)
+        self.animtor.add('asteroid2', 1, 2, 2)
+        self.animtor.add('asteroid3', 1, 3, 3)
+
+        self.lazer_sprite = pygame.image.load('./graphics/lazer.png').convert_alpha()
+
+    def spawn_asteroid(self, camera, scale, a_pos, player, dt):
+        at_pos = pygame.math.Vector2(a_pos)
+        player_pos = pygame.math.Vector2(player)
+        angle = math.atan2(at_pos.y - player_pos.y , at_pos.x - player_pos.x)
+        rand_speed = randint(100, 300)
+        rand_sprite = choice(['asteroid1', 'asteroid2', 'asteroid3'])
+        self.animtor.play(rand_sprite)
+        
+        Projectile(camera, at_pos, angle,  rand_speed, scale)
+        Projectile(camera, at_pos, angle, rand_speed, scale, self.animtor.get_frame().image, SPRITE_TYPE_ASTEROID, dt)
+
+    def spawn_rand_asteroid(self, camera, player, dt):
+        player_pos = pygame.math.Vector2(player)
+        spawnwidth , spawnheight = CENTER_SCREEN + 300, HEIGHT // 2 + 300
+        rand_x = randint(player_pos.x - spawnwidth, player_pos.x + spawnwidth)
+        rand_y = randint(player_pos.y - spawnheight, player_pos.y + spawnheight)
+        rand_pos = pygame.math.Vector2(rand_x, rand_y)
+        rand_scale = choice([1, 1, 1, 1.2])
+        self.spawn_asteroid(camera, rand_scale, rand_pos, player, dt)
