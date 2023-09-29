d1b9992314ed46d8f52ee17a7f5ece4e12142eed
barrales-d <dbarrales06@gmail.com>
2023-09-29 10:03:14 -0700

    Created spritesheet animator in pygame!

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import pygame
 import math
 from colors import *
 from settings import *
+from src.animator import *
 
 
 MAX_DRIFT_TIMER = 5
@@ -11,17 +12,17 @@ class Player(pygame.sprite.Sprite):
         self.type = SPRITE_TYPE_PLAYER
         self.angle = 0
         self.prev_angle = self.angle
-        self.float = pygame.transform.scale_by(pygame.image.load('./graphics/ship/float.png').convert_alpha(), 4)
-        self.shoot = pygame.transform.scale_by(pygame.image.load('./graphics/ship/shoot.png').convert_alpha(), 4)
-        self.boost = pygame.transform.scale_by(pygame.image.load('./graphics/ship/boost2.png').convert_alpha(), 4)
-        self.boost1 = pygame.transform.scale_by(pygame.image.load('./graphics/ship/boost1.png').convert_alpha(), 4)
-        self.animation = [self.boost, self.boost1]
-        self.boost_index = 0
 
-        self.unrotated_image =  self.float
+        self.animator = Animator('./graphics/ship_spritesheet_outline.png', 32, 3, 3, pos)
+        self.animator.add('idle', 2, start_frame=1, end_frame=1)
+        self.animator.add('fly', 5, end_frame=5)
+        self.animator.add('shoot', 3, start_frame=6)
+        self.animator.play('idle')
+
+        self.unrotated_image = pygame.transform.scale_by(self.animator.get_frame().image, SPRITE_SCALER)
         self.unrotated_rect = self.unrotated_image.get_rect(center = pos)
         self.image = self.unrotated_image
-        self.image.set_colorkey(black)
+        self.image.set_colorkey(SPRITE_COLOR_KEY)
         self.rect = self.image.get_rect(center = pos)
         self.rotate_image()
 
@@ -31,7 +32,7 @@ class Player(pygame.sprite.Sprite):
 
     def rotate_image(self):
         self.image = pygame.transform.rotate(self.unrotated_image, self.angle * 180 / math.pi)
-        self.image.set_colorkey(black)
+        self.image.set_colorkey(SPRITE_COLOR_KEY)
         self.rect.centerx = self.unrotated_rect.centerx - (self.image.get_width() // 2)
         self.rect.centery = self.unrotated_rect.centery - (self.image.get_height() // 2)
 
@@ -53,28 +54,31 @@ class Player(pygame.sprite.Sprite):
         dx = pygame.math.lerp(0, drift_x_speed, t)
         dy = pygame.math.lerp(0, drift_y_speed, t)
 
-        self.unrotated_image =  self.float
+        self.animator.play('idle')
+
         if keys[pygame.K_j]:
-            self.unrotated_image =  self.shoot
+            self.animator.play('shoot')
         
         speed_boost = 1
         if keys[pygame.K_LSHIFT]:
             speed_boost = 1.5
+        
 
-        if keys[pygame.K_w] or keys[pygame.K_UP]:
-            self.boost_index = (self.boost_index + 0.1) % len(self.animation)
-            self.unrotated_image =  self.animation[int(self.boost_index)]
-             
+        if keys[pygame.K_w] or keys[pygame.K_UP]:             
             dx = (math.sin(self.angle) * self.speed.x) * dt * speed_boost
             dy = (math.cos(self.angle) * self.speed.y) * dt * speed_boost
             self.prev_angle = self.angle
             self.drift_timer = MAX_DRIFT_TIMER
+            self.animator.play('fly')
 
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.angle += dt * self.rotation_speed
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.angle -= dt * self.rotation_speed
         self.angle %= (2 * math.pi)
+
+        self.animator.update()
+        self.unrotated_image = pygame.transform.scale_by(self.animator.get_frame().image, SPRITE_SCALER)
 
         self.unrotated_rect.x += dx
         self.unrotated_rect.y += dy
